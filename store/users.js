9b8c7a1a6d602eb4cb1c7958ce76357b263b5889
hunafazaky <hunafa.zaky@gmail.com>
2023-02-01 07:23:12 +0700

    update

--- store/users.js
+++ store/users.js
@@ -1,33 +1,32 @@
 export const state = () => ({
-  data: [
-    {
-      id: 1,
-      account: {
-        username: 'hunafazaky',
-        password: '12345678',
-      },
-      profile: {
-        image_profile: '/temp-profile.webp',
-        name: "Zaky Syi'ar Hunafa Nugroho",
-        birthdate: '13 September 2000',
-        place: 'Bandung',
-        bio: 'Mahasiswa UIN Sunan Gunung Djati Bandung',
-      },
-      activities: {
-        posts: [1, 3, 4, 5, 6],
-        favorite_post: [1, 2, 3, 4],
-        readings: [1, 3, 4, 5, 6],
-        writer_subs: [2, 3],
-        reader_subs: [2, 3],
-        bookshelf: [1, 2, 3, 4],
-      },
-      created_at: '19 April 2022',
-      updated_at: '19 April 2022',
-    },
-  ],
+  data: [],
 })
 
+export const getters = {
+  GetSpecificUser: state => id => {
+    return state.users.filter(q => q._id == id)[0]
+  }
+}
+
 export const mutations = {
+  GetRandomUser (state) {
+    fetch("http://localhost:3000/users/random")
+      .then(res => res.json())
+      .then(data => {
+        if (state.user._id != data._id) {
+          state.user = data;
+        } else {
+          this.commit('GetRandomUser');
+        }
+      });
+  },
+  GetAllUsers (state) {
+    fetch("http://localhost:3000/users")
+      .then(res => res.json())
+      .then(data => {
+        state.users = data;
+      });
+  },
   add(state, user) {
     state.data.push({
       user,
