d2f09bd3c5cdb6a17e1082f8c8956d11061f2936
hunafazaky <hunafa.zsn@gmail.com>
2026-05-23 22:50:04 +0700

    move base api to dotenv

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,8 +61,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "https://hz-reading.onrender.com/api",
-    // baseURL: 'https://pojokbaca-ucw8tnmp.b4a.run/api'
+    baseURL: process.env.API_BASE_URL || "http://localhost:8080/api",
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
